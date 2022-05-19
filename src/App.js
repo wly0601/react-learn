316f1c76de6067bc11b5e4acec404dbdfa0963da
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2022-05-19 13:51:13 +0700

    Upload Daily Task 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
 
 function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [showText, setShowText] = useState(false);
+  const sum = count1 + count2;
+  const [name, setName] = useState("Guest")
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Hello, {name}!</h1>
+        <div className="App-grid-container">
+          <div className="App-grid-item">
+            <button className="App-button" onClick={() => setName("Anya Forger")}>Anya Forger</button>
+          </div>
+          <div className="App-grid-item">
+            <button className="App-button" onClick={() => setName("Loid Forger")}>Loid Forger</button>
+          </div>
+          <div className="App-grid-item">
+            <button className="App-button" onClick={() => setName("Yor Forger")}>Yor Forger</button>
+          </div>  
+        </div>
+        
+        <h1> Let's Play Some Math! </h1>
+          <div className="App-grid-container">
+            <div className="App-grid-item">
+              <h4> First Number : </h4>
+              <h3> {count1} </h3>
+              <button className="App-button" onClick={() => {setCount1(count1 + 1); setShowText(false)}}>Click Me!</button>
+            </div>
+            <div className="App-grid-item">
+              <h4> Second Number : </h4>
+              <h3> {count2} </h3>
+              <button className="App-button" onClick={() => {setCount2(count2 + 1); setShowText(false)}}>Click Me!</button>
+            </div>
+            <div className="App-grid-item">
+              <h4> Sum of Those Numbers : </h4>
+              <h3> {count1} + {count2} =  {showText ? sum : null}  </h3>
+              <button className="App-button" onClick={() => setShowText(true)}>Reveal Answer!</button>
+            </div>
+          </div> 
       </header>
     </div>
   );
